0b1fbc0ef4276e1c6e306e95dbb0f3228f3b0017
pranav.kumar@iongroup.com <pranav.kumar@iongroup.com>
2019-04-01 17:29:49 +0530

    Updated Sql files

--- src/main/java/com/livetechstudy/springjdbc/SpringjdbcApplication.java
+++ src/main/java/com/livetechstudy/springjdbc/SpringjdbcApplication.java
@@ -6,6 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringjdbcApplication {
 
+	// H2 Console URL http://localhost:8080/h2-console
+	//JDBC URL jdbc:h2:mem:testdb
 	public static void main(String[] args) {
 		SpringApplication.run(SpringjdbcApplication.class, args);
 	}
